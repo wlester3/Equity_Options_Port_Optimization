28e0bc6de46de2e2f82b838494acadb5b5a54da7
wlester3 <98128677+wlester3@users.noreply.github.com>
2024-04-17 23:36:02 -0400

    Project_Start
    Gathered introductory data using yfinance, implemented an initial 'max return, lowest vol' optimization mode, and began portfolio simulation implementation.

---
+++ visualizations.py
@@ -0,0 +1,34 @@
+
+import numpy as np, pandas as pd, matplotlib.pyplot as plt
+from fredapi import Fred
+import yfinance as yf
+
+
+def plot_cumulative_returns(log_returns):
+    # Calculate cumulative log returns
+    cumulative_log_returns = log_returns.cumsum()
+    cumulative_returns = np.exp(cumulative_log_returns) - 1
+    
+    plt.figure(figsize=(12, 8))
+    for column in cumulative_returns.columns:
+        plt.plot(cumulative_returns.index, cumulative_returns[column], label=column)
+
+    plt.title('Cumulative Returns Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Cumulative Returns')
+    plt.legend(title="Stocks", loc='upper center', bbox_to_anchor=(0.5, -0.08), ncol=len(cumulative_returns.columns)/2)
+    plt.grid(True)
+    plt.show()
+
+
+
+def plot_paths(S, with_mean=False):
+    plt.figure(figsize=(10, 6))
+    plt.plot(S.T, lw=1)
+    if with_mean:
+        plt.plot(S.mean(axis=0), 'k', lw=2, label='Mean Path')
+        plt.legend()
+    plt.title('GBM Simulations')
+    plt.xlabel('Time Steps')
+    plt.ylabel('Stock Price')
+    plt.show()
